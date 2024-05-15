abb9e91b0180d4d3cd0cfed4274aef5182211c98
otavio-Pucharelli <otaviog.m.p@gmail.com>
2024-05-15 11:50:04 -0300

    initial commit

---
+++ cmd/web/routes.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/otavio-Pucharelli/filhos-da-luz/pkg/config"
+	"github.com/otavio-Pucharelli/filhos-da-luz/pkg/handlers"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
+	mux.Use(SessionLoad)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/resident", handlers.Repo.Resident)
+
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
+	return mux
+}
